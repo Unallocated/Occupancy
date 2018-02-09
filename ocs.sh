795f7ddd0e5f1244e5adb3a0d0fc9cda6ec61cd2
UsakoSenpai <usagi629@gmail.com>
2018-02-08 23:24:00 -0500

    Changed to sftp for wall photo to website.

--- ocs.sh
+++ ocs.sh
@@ -70,7 +70,7 @@ END_FTP_COMMANDS
 pushWallToWebsite ()
 {
     stamp=$(date '+%F_%T')
-    ftp -n "${OCS_UAS_URL}" << END_FTP_COMMANDS
+    sftp -n "${OCS_UAS_URL}" << END_FTP_COMMANDS
         quote USER ${OCS_UAS_USER}
         quote PASS ${OCS_UAS_PASS}
         ascii
